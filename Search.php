e9ae078c0ff5ac17242361dab87964df18dfca16
M@lith077 <malith.git@gmail.com>
2020-05-31 18:30:20 +1000

    First commit

---
+++ Search.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+    <meta charset="utf-8">
+    </head>
+
+    <body>
+
+      <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="C:\Users\Noah\Desktop\TWA Task\Play.php">Play</a>
+        <a href="C:\Users\Noah\Desktop\TWA Task\Playlist.php">Playlist</a>
+        <a href="C:\Users\Noah\Desktop\TWA Task\Login.php">Login</a>
+        <a href="C:\Users\Noah\Desktop\TWA Task\Logout.php">Logout</a>
+      </div>
+
+    </body>
+    </html>
